934cabd3bfee99853752eaea1a880dfa640d17ed
Iury Vasconcelos <iury0393@gmail.com>
2019-12-02 22:48:39 -0300

    Commit inicial

---
+++ Assets/Scripts/DialogueRuby.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueRuby : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    [TextArea(3, 10)]
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    public GameObject continueButton;
+    public GameObject dialogueBox;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    void Update()
+    {
+        if(textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.Z))
+            {
+                NextSentence();
+            }
+            
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        continueButton.SetActive(false);
+
+        if(index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        } else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+            dialogueBox.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/ToPreviousScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToPreviousScene : MonoBehaviour
+{
+    private int prevSceneToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevSceneToLoad = SceneManager.GetActiveScene().buildIndex - 1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(prevSceneToLoad);
+    }
+}
